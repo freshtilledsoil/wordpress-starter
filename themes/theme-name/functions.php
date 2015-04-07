2e98b5ec43e16dd03532589dc43846657f26c050
Tim Wright <timwright12@gmail.com>
2015-04-07 10:12:53 -0400

    Option to remove menu items

--- themes/theme-name/functions.php
+++ themes/theme-name/functions.php
@@ -155,6 +155,27 @@ remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 );
 
 /*
+  Remove menu items if the client doesn't need them
+*/
+
+function remove_menus() {
+/*
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'upload.php' );                 //Media
+  remove_menu_page( 'edit.php?post_type=page' );    //Pages
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  remove_menu_page( 'themes.php' );                 //Appearance
+  remove_menu_page( 'plugins.php' );                //Plugins
+  remove_menu_page( 'users.php' );                  //Users
+  remove_menu_page( 'tools.php' );                  //Tools
+  remove_menu_page( 'options-general.php' );        //Settings
+*/
+}
+
+add_action( 'admin_menu', 'remove_menus' );
+
+/*
   Cleaning up Navigation classes
 */
 
