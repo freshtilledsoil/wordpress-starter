ce9ed82b987682e76a01b5a17adbda392632c9d9
Tim Wright <timwright12@gmail.com>
2015-04-14 15:51:00 -0400

    new assets director structure and turned on post-thumbnail by default

--- themes/theme-name/functions.php
+++ themes/theme-name/functions.php
@@ -46,7 +46,7 @@ function theme_name_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -102,9 +102,7 @@ add_action( 'widgets_init', 'theme_name_widgets_init' );
 function theme_name_scripts() {
 	wp_enqueue_style( 'theme-name-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'theme-name-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'theme-name-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'theme-name-skip-link-focus-fix', get_template_directory_uri() . '/assets/javascripts/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
